f078c9965d0bc86e11146ba287c87e22679235f3
Wohnlich, Eric (IMS) <wohnliche@imsweb.com>
2015-08-26 16:17:04 -0400

    this may be crazy

--- collective/tinymceplugins/latex/future.py
+++ collective/tinymceplugins/latex/future.py
@@ -1,15 +1,28 @@
 from Products.Five.browser import BrowserView
 
-class RenderLatexExample(BrowserView):
-    def get_image(self):
+class RenderLatex(BrowserView):
+    def __call__(self):
       try:
         import matplotlib
-        matplotlib.use('Agg')
+        matplotlib.use('Agg') # png
         import matplotlib.pyplot as plt
       except:
         return None
 
-      a = '\\frac{a}{b}'  #notice escaped slash
-      plt.plot()
-      plt.text(0.5, 0.5,'$%s$'%a)
-      return plt.show()
+      formula = self.request.get('f') or self.request.get('f')
+
+      fig = plt.figure(figsize=(3,3))
+
+      buf = io.BytesIO()
+      def save(buf)):
+        fig.savefig(buf,bbox_inches='tight',pad_inches=0)
+
+      fig.text(0, 0, formula)
+      save(buf); buf.seek(0) # render once so we can get size
+      txtw = fig.texts[0].get_window_extent()
+      fig.set_figheight((txtw.y1-txtw.y0)/fig.get_dpi())
+      fig.set_figwidth((txtw.x1-txtw.x0)/fig.get_dpi())
+      save(buf)
+
+      self.request.response.setHeader('Content-Type','image/png')
+      return buf.read()

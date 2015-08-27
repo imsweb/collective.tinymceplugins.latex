081839d762b2d5461dcb4dc697eaa1a2428b208c
Wohnlich, Eric (IMS) <wohnliche@imsweb.com>
2015-08-27 11:23:32 -0400

    I'm a wizard, Harry!

--- collective/tinymceplugins/latex/future.py
+++ collective/tinymceplugins/latex/future.py
@@ -11,19 +11,20 @@ class RenderLatex(BrowserView):
 
       formula = self.request.get('formula') or self.request.get('f')
 
-      fig = plt.figure(figsize=(3,3))
+      fig = plt.figure(figsize=(0.1,0.1))
 
       buf = io.BytesIO()
       def save(buf):
         fig.savefig(buf,bbox_inches='tight',pad_inches=0)
 
-      fig.text(0, 0, '$%s$' % formula)
-      save(buf); del buf # render once so we can get size
-      buf = io.BytesIO()
-      txtw = fig.texts[0].get_window_extent()
-      fig.set_figheight((txtw.y1-txtw.y0)/fig.get_dpi())
-      fig.set_figwidth((txtw.x1-txtw.x0)/fig.get_dpi())
+      fig.text(0, 0, '$%s$' % formula, size=16)
       save(buf)
+      #; del buf # render once so we can get size
+      #buf = io.BytesIO()
+      #txtw = fig.texts[0].get_window_extent()
+      #fig.set_figheight((txtw.y1-txtw.y0)/fig.get_dpi())
+      #fig.set_figwidth((txtw.x1-txtw.x0)/fig.get_dpi())
+      #save(buf)
 
       buf.seek(0)
       self.request.response.setHeader('Content-Type','image/png')

862abbbc4a95e8a58b856fc4b1a801860fa6cca1
Wohnlich, Eric (IMS) <wohnliche@imsweb.com>
2015-08-27 10:48:10 -0400

    magic

--- collective/tinymceplugins/latex/future.py
+++ collective/tinymceplugins/latex/future.py
@@ -13,16 +13,18 @@ class RenderLatex(BrowserView):
 
       fig = plt.figure(figsize=(3,3))
 
-      buf = io.BytesIO()
+
       def save(buf):
         fig.savefig(buf,bbox_inches='tight',pad_inches=0)
 
-      fig.text(0, 0, formula)
-      save(buf); buf.seek(0) # render once so we can get size
+      fig.text(0, 0, '$%s$' % formula)
+      save(buf); del buf # render once so we can get size
+      buf = io.BytesIO()
       txtw = fig.texts[0].get_window_extent()
       fig.set_figheight((txtw.y1-txtw.y0)/fig.get_dpi())
       fig.set_figwidth((txtw.x1-txtw.x0)/fig.get_dpi())
       save(buf)
 
+      buf.seek(0)
       self.request.response.setHeader('Content-Type','image/png')
       return buf.read()

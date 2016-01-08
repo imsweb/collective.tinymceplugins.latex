88e162e9b1ca472bea1df38d48b4f577f81feb4e
Wohnlich, Eric (IMS) <wohnliche@imsweb.com>
2016-01-08 14:30:10 -0500

    initial p5 upgrade

--- collective/tinymceplugins/latex/__init__.py
+++ collective/tinymceplugins/latex/__init__.py
@@ -1,5 +1,2 @@
-from Products.CMFCore.DirectoryView import registerDirectory
 from zope.i18nmessageid import MessageFactory
 _ = MessageFactory('collective.tinymceplugins.latex')
-
-registerDirectory('skins', globals())

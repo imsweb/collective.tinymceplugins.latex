cb70d1f56ba4bff88d7b6107e907cd92784db29c
Wohnlich, Eric (IMS) <wohnliche@imsweb.com>
2016-03-16 14:50:29 -0400

    Revert "initial p5 upgrade"
    This reverts commit 88e162e9b1ca472bea1df38d48b4f577f81feb4e

--- collective/tinymceplugins/latex/__init__.py
+++ collective/tinymceplugins/latex/__init__.py
@@ -1,2 +1,5 @@
+from Products.CMFCore.DirectoryView import registerDirectory
 from zope.i18nmessageid import MessageFactory
 _ = MessageFactory('collective.tinymceplugins.latex')
+
+registerDirectory('skins', globals())

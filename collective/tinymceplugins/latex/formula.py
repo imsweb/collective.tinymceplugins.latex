d98589f568fe1d9043dff94ceab300d0ba4554d9
Wohnlich, Eric (IMS) <wohnliche@imsweb.com>
2017-02-06 16:47:23 -0500

    just pycharm format cleanup, not even going to release these yet

--- collective/tinymceplugins/latex/formula.py
+++ collective/tinymceplugins/latex/formula.py
@@ -1,34 +1,35 @@
 import cgi
 from Products.Five.browser import BrowserView
 
+
 class RenderLatex(BrowserView):
     def __call__(self):
-      try:
-        import matplotlib, io
-        matplotlib.rcParams['text.latex.preamble'].append(r'\usepackage{amsmath}')
-        matplotlib.use('Agg') # png
-        import matplotlib.pyplot as plt
-
-        formula = self.request.get('formula') or self.request.get('f')
-        size = self.request.get('size') or self.request.get('s') or 16
-        if not formula:
-          return
         try:
-          size = int(size)
-        except ValueError: #invalid int
-          size = 16
+            import matplotlib, io
+            matplotlib.rcParams['text.latex.preamble'].append(r'\usepackage{amsmath}')
+            matplotlib.use('Agg')  # png
+            import matplotlib.pyplot as plt
 
-        buf = io.BytesIO()
-        fig = plt.figure(figsize=(0.1,0.1))
-        fig.text(0, 0, '$%s$' % formula, size=size)
-        fig.savefig(buf,bbox_inches='tight',pad_inches=0)
+            formula = self.request.get('formula') or self.request.get('f')
+            size = self.request.get('size') or self.request.get('s') or 16
+            if not formula:
+                return
+            try:
+                size = int(size)
+            except ValueError:  # invalid int
+                size = 16
 
-        buf.seek(0)
-        self.request.response.setHeader('Content-Type','image/png')
-        return buf.read()
-      except Exception, e:
-        return 'error: %s' % cgi.escape(e.message).replace('\n','<br/>')
+            buf = io.BytesIO()
+            fig = plt.figure(figsize=(0.1, 0.1))
+            fig.text(0, 0, '$%s$' % formula, size=size)
+            fig.savefig(buf, bbox_inches='tight', pad_inches=0)
+
+            buf.seek(0)
+            self.request.response.setHeader('Content-Type', 'image/png')
+            return buf.read()
+        except Exception, e:
+            return 'error: %s' % cgi.escape(e.message).replace('\n', '<br/>')
 
     def absolute_url(self):
-      """ Appease the plone.outputfilters gods """
-      return self.context.absolute_url()+'/@@latex'
+        """ Appease the plone.outputfilters gods """
+        return self.context.absolute_url() + '/@@latex'

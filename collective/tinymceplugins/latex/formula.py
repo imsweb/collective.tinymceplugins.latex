0a8f12cf0eb1f21e7577d88b638fe9bd057c416c
Wohnlich, Eric (IMS) <wohnliche@imsweb.com>
2017-12-15 18:19:35 +0000

    factor out the code that renders an image and memoize that method https://pypi.python.org/pypi/plone.memoize#view-and-instance

--- collective/tinymceplugins/latex/formula.py
+++ collective/tinymceplugins/latex/formula.py
@@ -1,15 +1,30 @@
-from Products.Five.browser import BrowserView
 import cgi
+import io
+
+from Products.Five.browser import BrowserView
+from plone.memoize.instance import memoize
 
 
 class RenderLatex(BrowserView):
+    @memoize
+    def render_image(self, formula, size):
+        import matplotlib.pyplot as plt
+        buf = io.BytesIO()
+        fig = plt.figure(figsize=(0.1, 0.1))
+        fig.text(0, 0, '$%s$' % formula, size=size)
+        fig.savefig(buf, bbox_inches='tight', pad_inches=0)
+
+        buf.seek(0)
+        self.request.response.setHeader('Content-Type', 'image/png')
+        data = buf.read()
+        plt.close()
+        return data
+
     def __call__(self):
         try:
             import matplotlib
-            import io
             matplotlib.rcParams['text.latex.preamble'].append(r'\usepackage{amsmath}')
             matplotlib.use('Agg')  # png
-            import matplotlib.pyplot as plt
 
             formula = self.request.get('formula') or self.request.get('f')
             size = self.request.get('size') or self.request.get('s') or 16
@@ -20,16 +35,7 @@ class RenderLatex(BrowserView):
             except ValueError:  # invalid int
                 size = 16
 
-            buf = io.BytesIO()
-            fig = plt.figure(figsize=(0.1, 0.1))
-            fig.text(0, 0, '$%s$' % formula, size=size)
-            fig.savefig(buf, bbox_inches='tight', pad_inches=0)
-
-            buf.seek(0)
-            self.request.response.setHeader('Content-Type', 'image/png')
-            data = buf.read()
-            plt.close()
-            return data
+            return self.render_image(formula, size)
         except Exception, e:
             return 'error: %s' % cgi.escape(e.message).replace('\n', '<br/>')
 

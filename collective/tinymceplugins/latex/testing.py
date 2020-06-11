41701a4d020b27574151015a380d2df09df568b4
wohnlice <wohnlice@imsweb.com>
2020-06-11 19:19:22 -0400

    flake

--- collective/tinymceplugins/latex/testing.py
+++ collective/tinymceplugins/latex/testing.py
@@ -4,7 +4,6 @@ from plone.app.testing import IntegrationTesting
 from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import PloneSandboxLayer
 from plone.app.testing import applyProfile
-from plone.testing import z2
 
 
 class TinyMceLatexLayer(PloneSandboxLayer):
